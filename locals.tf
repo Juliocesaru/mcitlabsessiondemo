f0b125d435d27de7a2df174469bf3d181e5a0ab2
Juliocesaru <168040425+Juliocesaru@users.noreply.github.com>
2024-04-26 20:40:02 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -3,9 +3,15 @@ locals{
   forum="abracadabramcit"
   lengthsa=length(local.servicename)
   lengthforum=length(local.forum)
+  firstname="julio"
+  lastname="urueta"
+  city="Montreal"
 }
 output "print"{
     value = local.servicename
+}
+output "print"{
+    value = city
 }
 output "secondprint"{
     value = local.forum
