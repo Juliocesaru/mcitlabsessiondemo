52878e46d47e10a310cffffbf920b8fe5e17be23
Juliocesaru <168040425+Juliocesaru@users.noreply.github.com>
2024-04-29 20:51:51 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -65,7 +65,8 @@ variable "restaurants"{
 output "restaurantslist"{
     value=[for restaurants in var.restaurants:restaurants ]
 }
-total_output = ["175","175","175"]
+total_output = ["150","150","150"]
+
 output "sumofthreenumbers"{
     value=sum([for numberoutput in local.total_output: tonumber(numberoutput)])
 }
