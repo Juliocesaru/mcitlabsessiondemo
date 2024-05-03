6acf4aab7bec26cb483b0748c08447ba94833a3e
Juliocesaru <168040425+Juliocesaru@users.noreply.github.com>
2024-05-03 19:14:25 -0400

    Update homwrkw.tf

--- homwrkw.tf
+++ homwrkw.tf
@@ -1,16 +1,12 @@
-variable "countries_list"{
-   default = ["Mexico","Greece","Italy","Germany","Portugal"]
-  }
-
-output "countries_as_string"{
-  value = join("+",var.countries_list)
+variable "simple_nested_list"{
+  type    = list(list(string))
+  default = [["a","b"],["c","d"],["e","f"]]
 }
-
-output "is_Canada_in_list"{
-  value = contains(var.countries_list,"Canada")
-  }
-
-output "merged_string"{
-  value = trim("I love" + "Canada"," ")
+output "flattened-list"{
+value = flatten(var.simple_nested_list)
+}
+variable "nested_list"{
+  type = list(list(string))
+  default = [["a","b"],["c","d"],["e","f"]]
 }
 
