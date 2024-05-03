ca48e7f859271f220a698e9e492f1ef073d4c37f
Juliocesaru <168040425+Juliocesaru@users.noreply.github.com>
2024-05-03 19:22:28 -0400

    Update homwrkw.tf

--- homwrkw.tf
+++ homwrkw.tf
@@ -9,6 +9,6 @@ variable "nested_list"{
   type = list(list(string))
   default = [["a","b"],["c","d"],["e","f"]]
 }
-output "simple_nested_list"{
-  simple_local_flattened_list = flatten(var.simple_nested_list)
- }
+output "nested_list"{
+  value = flatten(var.simple_nested_list)
+}
