5ff0e3968825802a30dde65fa35eb5ea6a97da78
Juliocesaru <168040425+Juliocesaru@users.noreply.github.com>
2024-05-03 18:26:13 -0400

    Create homwrkw.tf

---
+++ homwrkw.tf
@@ -0,0 +1,16 @@
+variable "countries_list"{
+   default = ['Mexico","Greece","Italy","Germany","Portugal"]
+  }
+
+output "countries_as_string"{
+  value = join("+",var.countries_list)
+}
+
+output "is_Canada_in_list"{
+  value = contains(var.countries_list,"Canada")
+  }
+
+output "merged_string"{
+  value = trim("i love" + "Canada"," ")
+}
+
