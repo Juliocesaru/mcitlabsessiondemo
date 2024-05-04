8e20e16b24fca422cc61cc5b332d7def1843b2c8
Juliocesaru <168040425+Juliocesaru@users.noreply.github.com>
2024-05-03 20:28:20 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -60,6 +60,29 @@ output "flattened_list" {
   value = local.flattened_list
   description = "A flattened list of all elements."
 }
-
+variable "user_roles" {
+  type    = map(string)
+  default = {
+    alice = "admin"
+    bob   = "editor"
+    charlie = "viewer"
+  }
+}
+variable "default_role" {
+  type    = string
+  default = "guest"
+}
+ 
+locals {
+  user_role_lookup = {
+    alice   = lookup(var.user_roles, "alice", var.default_role)
+    bob     = lookup(var.user_roles, "bob", var.default_role)
+    david   = lookup(var.user_roles, "david", var.default_role)
+  }
+}
+ 
+output "user_roles_result" {
+  value = local.user_role_lookup
+}
 
 
