87c3cfc196a547fb8d67f1ce7216d70a3884bca6
Juliocesaru <168040425+Juliocesaru@users.noreply.github.com>
2024-05-03 20:03:35 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,3 +2,7 @@ output "character_enemy_map" {
   value = local.character_enemy_map
   description = "Mapping of characters to the number of enemies they've destroyed."
 }
+output "sum_of_numbers" {
+  value = sum(local.numbers_as_numbers)
+  description = "The sum of the provided numbers."
+}
