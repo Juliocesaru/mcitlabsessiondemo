9da74089bc4118827a224f35d4a450db3d8ec5be
Juliocesaru <168040425+Juliocesaru@users.noreply.github.com>
2024-05-06 18:00:15 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -6,16 +6,17 @@ output "sum_of_numbers" {
   value = sum(local.numbers_as_numbers)
   description = "The sum of the provided numbers."
 }
-locals {
-  incremented_output = [for i in var.total_output : tostring(tonumber(i) + 10)]
-}
 
 output "incremented_output" {
   value = local.incremented_output
 }
-output "sports_list_evaluation" {
-  value = length(var.winterlistOfSports) > 3 ? "Winter sports list is extensive" : "Winter sports list is limited"
-}
 output "nested_map" {
   value = var.nested_map
+}
+locals {
+  incremented_output = [for i in var.total_output : tostring(tonumber(i) + 10)]
+}
+
+output "sports_list_evaluation" {
+  value = length(var.winterlistOfSports) > 3 ? "Winter sports list is extensive" : "Winter sports list is limited"
 }
