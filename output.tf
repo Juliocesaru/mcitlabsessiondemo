2c02b693c909166dc858134e9185d333e7f73a5e
Juliocesaru <168040425+Juliocesaru@users.noreply.github.com>
2024-05-06 17:37:32 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -6,3 +6,10 @@ output "sum_of_numbers" {
   value = sum(local.numbers_as_numbers)
   description = "The sum of the provided numbers."
 }
+locals {
+  incremented_output = [for i in var.total_output : tostring(tonumber(i) + 10)]
+}
+
+output "incremented_output" {
+  value = local.incremented_output
+}
