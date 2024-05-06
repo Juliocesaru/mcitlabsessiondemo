48c6d5ac298e755e2df6316d349106923a058cb2
Juliocesaru <168040425+Juliocesaru@users.noreply.github.com>
2024-05-06 19:12:29 -0400

    Update exam1.tf

--- exam1.tf
+++ exam1.tf
@@ -9,3 +9,13 @@ output "updated_clouds" {
 output "check_azure" {
   value = contains(local.clouds, "azure") ? "Azure is part of the cloud list" : "Azure is not part of the cloud list"
 }
+
+locals {
+  clouds       = ["azure", "aws", "gcp"]
+  cloud_owners = ["Microsoft", "Amazon", "Google"]
+  cloud_map    = zipmap(local.clouds, local.cloud_owners)
+}
+
+output "cloud_map" {
+  value = local.cloud_map
+}
