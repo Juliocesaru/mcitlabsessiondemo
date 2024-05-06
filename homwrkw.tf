f9d377f2e0ac8ee47de4f0e2e24f2ee37dedab03
Juliocesaru <168040425+Juliocesaru@users.noreply.github.com>
2024-05-06 17:53:00 -0400

    Update homwrkw.tf

--- homwrkw.tf
+++ homwrkw.tf
@@ -12,12 +12,3 @@ variable "nested_list"{
 output "nested_list"{
   value = flatten(var.simple_nested_list)
 }
-variable "nested_map"{
-  type    = map(map(string))
-  default = {
-    group1 = {"name" = "Peter", "age" = "28"}
-    group2 = {"name" = "Lukas", "age" = "32"}
-    group3 = {"name" = "Luis", "age" = "23"}
-    group4 = {"name" = "Kane", "age" = "35"}
-}
-}
