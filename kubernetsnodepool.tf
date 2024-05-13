4f2ff1b89bcc79b0c8472543efa8e1e637ac1e92
Juliocesaru <168040425+Juliocesaru@users.noreply.github.com>
2024-05-13 18:57:22 -0400

    Update kubernetsnodepool.tf

--- kubernetsnodepool.tf
+++ kubernetsnodepool.tf
@@ -1,1 +1,11 @@
-resourse "a
+resourse "azurerm_kubernets_cluster_node_pool" 'kubenode1pool"{
+for_each                = azurerm_kubernets_cluster.batchabcd
+name                    = "${each.key}"
+kubernets_cluster_id    = each.value.id
+vm_size                 = "Standar_DS2_V2"
+node_count              = 1
+
+tags = {
+  Environment = "Production"
+ }
+}
