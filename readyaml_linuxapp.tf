fe3cd6bea29c9f2b7ec3bcceac3258f11767e265
Juliocesaru <168040425+Juliocesaru@users.noreply.github.com>
2024-05-13 19:05:28 -0400

    Update readyaml_linuxapp.tf

--- readyaml_linuxapp.tf
+++ readyaml_linuxapp.tf
@@ -1,4 +1,4 @@
-llocals{
+locals{
   linux_app=[for f in fileset("${path.module}/configs", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
   linux_app_list = flatten([
     for app in local.linux_app : [
